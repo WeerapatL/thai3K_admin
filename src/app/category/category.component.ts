95d27d304352d531e840f8c016d36448ff04c6cd
mnkych <mnkych@gmail.com>
2018-01-03 00:10:44 +0700

    วนสินค้า

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -10,8 +10,9 @@ import { DataService } from '../dataprovider/DataService';
   styleUrls: ['../../vendor/bootstrap/css/bootstrap.min.css', '../../css/shop-homepage.css']
 })
 export class CategoryComponent implements OnInit {
-  text: string;
-  id: number
+  brand: string;
+  id: number;
+  CategoryComponent
   Product: Product[];
   constructor(private route: ActivatedRoute, public service: DataService, public router: Router) {
 
@@ -19,29 +20,57 @@ export class CategoryComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe(result => {
-      this.text = result['Brand'];
+      this.brand = result['Brand'];
       this.id = result['CategoryID'];
-      if (this.id != undefined) {
-        this.getCategoryProduct(this.id);
-      } else {
-        this.service.getBrandProduct(this.text).subscribe(result => {
-          this.Product = result;
-          console.log(this.Product);
-        });
+      console.log(this.id);
+      console.log(this.brand);
+      if (this.id != undefined || this.brand != undefined) {
+        if (this.id != undefined) {
+          this.getCategoryProduct(this.id);
+          document.getElementById('showCategory').innerHTML = document.getElementById(this.id.toString()).innerHTML;
+          document.getElementById('showCategory').style.display = "";
+          if (this.id >= 1 && this.id <= 8) {
+            this.brand = 'Zeberg';
+          }
+          if (this.id >= 9 && this.id <= 15) {
+            this.brand = 'Kanok';
+          }
+          if (this.id >= 16 && this.id <= 19) {
+            this.brand = 'Nano';
+          }
 
+        } else {
+          if (this.brand != undefined) {
+            this.service.getBrandProduct(this.brand).subscribe(result => {
+              this.Product = result;
+              console.log(this.Product);
+            });
+            document.getElementById('showCategory').style.display = "none";
+          }
+        }
+      } else {
+        this.router.navigate(['../Home']);
       }
+
     });
 
   }
   getCategoryProduct(CategoryParams: number) {
     this.service.getCategoryProduct(CategoryParams).subscribe(result => {
       this.Product = result;
+      
       console.log(this.Product);
     });
   }
-  selectedCategory(CategoryParams: number) {
+  selectedCategory(CategoryParams: number) {   
+   
+    this.router.navigate(['../Category', { Brand: this.brand, CategoryID: CategoryParams }]);
+  
+  }
+  selectedBrand(BrandParams:string) {
     // new CategoryComponent(str);
-    this.router.navigate(['../Category', { Brand: this.text, CategoryID: CategoryParams }]);
+    this.router.navigate(['../Category', { Brand: BrandParams}]);
+
   }
 
 }
