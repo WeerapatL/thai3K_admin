aa279e66c861777790d5445a8a107a11ab2a3f28
Pome <weerapat.laor@gmail.com>
2018-01-05 22:00:03 +0700

    commiy

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -4,18 +4,24 @@ import { DataService } from '../dataprovider/DataService';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['../../css/shop-item.css','../../vendor/bootstrap/css/bootstrap.min.css']
+  styleUrls: ['../../css/shop-item.css', '../../vendor/bootstrap/css/bootstrap.min.css']
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, 
-    public service: DataService, 
+  ///สร้าง Attr ตามหน้า insert 
+
+  constructor(private route: ActivatedRoute,
+    public service: DataService,
     public router: Router) { }
 
   ngOnInit() {
   }
 
-  selectedProduct(){
+  selectedProduct() {
     this.router.navigate(['../ProductDetail']);
+  }
+
+  updateProductDetail() {
+    //เมื่อแก้ไขเสร็จให้ส่งค่าไป update
   }
 }
